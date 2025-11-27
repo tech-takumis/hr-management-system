9fd895cc6da6e7ebaace03e035ad02203f4c0254
takumis <113819650+tech-takumis@users.noreply.github.com>
2025-11-27 20:46:01 +0800

    Done removing customer model, service, and controller for customer entity, also I already implemented authenticated state in the frontend

--- hr-management-frontend/vite.config.ts
+++ hr-management-frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import VueDevTools from 'vite-plugin-vue-devtools'
+// import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,6 +15,6 @@ export default defineConfig({
     },
     plugins: [
         vue(),
-        VueDevTools()
+        // VueDevTools()
     ],
 })
